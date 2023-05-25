3d9f48664f84cc4a1044613f0a6cf81cbe0f9f41
lintangc9 <lintangc.perwita@gmail.com>
2023-05-25 21:05:04 +0700

    Tester

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function hitungLuas() {
+    var sisiLuas = document.getElementById("sisi-luas").value;
+
+
+    var style = document.getElementsByClassName("result-luas")
+    document.getElementById("output-luas").innerHTML = '<span>'+sisiLuas+'</span>';
+    console.log(style);
+}
+
